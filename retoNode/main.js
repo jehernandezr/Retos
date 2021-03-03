4ddb26dc2dfceb52fb2e015f95c6fd8eb8aeaf3e
JONATAN ESMITS HERNANDEZ RUBIO+ <je.hernandezr@uniandes.edu.co>
2021-03-02 23:58:27 -0500

    fin

--- retoNode/main.js
+++ retoNode/main.js
@@ -1,35 +1,63 @@
 const axios= require("axios");
-const fs = require("fs");
+const fs = require("fs").promises;
 const http = require('http');
 
 
 const URL_proveedores="https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json"
 const URL_clientes="https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json"
 
-//Lee un archivo del sistema de archivos
-fs.readFile("app.js", (err, data) => {
-    console.log(data);
-  });
-  
-  //Crea un archivo en el sistema de archivos 
-  fs.writeFile("file.txt", "contenido", "utf-8", (err) => {
-    if (err) console.log("Error writing file");
-  });
 
+  const server = http.createServer(async function (req, res) {
+   
+    res.setHeader("Content-Type", "text/html");
+    switch(req.url){
+        case "/api/clientes": 
+        try {
+            let clientes= await axios.get(URL_clientes);
+            let dclientes= await JSON.stringify(clientes.data);
+            fs.readFile("./clientes.html","utf-8").then(contents => {
+                contents = contents.replace("{{data}}", dclientes).replace("{{id}}", "idCliente").replace("{{nombre}}", "NombreCompania").replace("{{contacto}}", "NombreContacto");
+                res.writeHead(200);
+                res.write(contents);
+                res.end();
+                
+            }).catch(err=>{console.log(err)});
+        }
+        catch (e){
+            console.log(e.message);
+        }
+            break;
+        case "/api/proveedores":
+            try {
+                let proveedores= await axios.get(URL_proveedores);
+                let dproveedores= await JSON.stringify(proveedores.data);
+                fs.readFile("./proveedores.html","utf-8").then(contents => {
+                    contents = contents.replace("{{data}}", dproveedores).replace("{{id}}", "idproveedor").replace("{{nombre}}", "nombrecompania").replace("{{contacto}}", "nombrecontacto");
+                    res.writeHead(200);
+                    res.write(contents);
+                    res.end();
+                    
+                }).catch(err=>{console.log(err)});
+            }
+            catch (e){
+                console.log(e.message);
+            }
+            break;
 
-http.createServer(function (req, res) {
-  
-    // Lee el archivo testfile.txt el cual se encuentra en la misma ruta que este script
-    fs.readFile('testfile.txt', 'utf8', function(err, data) {
-      if (err) throw err; // Retorna error si no encuentra el archivo
-      
-      // Encabezado de la respuesta del servidor
-      res.writeHead(200, {'Content-Type': 'text/html'}); 
+        case "/":
+            res.writeHead(200);
+            res.end(`<html><body><h1>HOME</h1> <a href="/api/clientes">Clientes</a> <a href="/api/proveedores">Proveedores</a></body></html>`);
+            break;
+        default:
+            res.writeHead(404);
+            res.end(``);
     
-      // Especifica el contenido que debe ser incluido en la respuesta
-      // Note que el objeto data ahora es parámetro de la función uc, alias del módulo upper-case
-      res.end(uc(data));
-    });
+    }
   
-  }).listen(8081); 
+  });
 
+  server.listen(8081, "localhost", () => {
+      console.log(`Server is running on http://localhost:8081`);
+  });
+
+  
